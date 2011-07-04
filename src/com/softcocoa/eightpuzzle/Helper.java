cecfd26b36fee9150ac5eac59bcb788787dbab7b
Porramate Lim <basicsharp@gmail.com>
2011-07-05 04:22:14 +0700

    Add solution checking for randomized initial states

--- src/com/softcocoa/eightpuzzle/Helper.java
+++ src/com/softcocoa/eightpuzzle/Helper.java
@@ -14,13 +14,23 @@ public class Helper {
 			tiles[i] = tiles[swapPosition];
 			tiles[swapPosition] = temp;
 		}
-		for (int i=0; i<C.NUMBER_OF_TILES; i++) {
-			int swapPosition = randomizer.nextInt(C.NUMBER_OF_TILES);
-			int temp = tiles[i];
-			tiles[i] = tiles[swapPosition];
-			tiles[swapPosition] = temp;
+		if(checkPuzzleHasSolution(tiles)) 
+			return tiles;
+		else			
+			return randomInitialPuzzleStateTiles();
+	}
+	
+	public static boolean checkPuzzleHasSolution(int tiles[]){
+		int countOdd = 0;
+		for (int i = 0; i < tiles.length; i++) {
+			for (int j = i+1; j < tiles.length; j++) {
+				if(tiles[i] > tiles[j] && tiles[i]!=0 && tiles[j]!=0){
+					countOdd++;
+				}
+			}
 		}
-		return tiles;
+		if(countOdd%2 == 0)	return false;
+		else return true;
 	}
 	
 	public static int searchTileInTiles(int tileValue, int tiles[]) {
